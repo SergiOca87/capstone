3cd531f1ec22fd601ff36c8f166f06f5b3fd7a36
ocasergi@gmail.com <ocasergi@gmail.com>
2020-11-28 22:41:12 +0100

    lots of updates

--- views.py
+++ views.py
@@ -22,7 +22,7 @@ def login_view(request):
         # Check if authentication successful
         if user is not None:
             login(request, user)
-            return HttpResponseRedirect(reverse("list"))
+            return HttpResponseRedirect(reverse("project_list.html"))
         else:
             return render(request, "index.html", {
                 "message": "Invalid username and/or password."
@@ -64,15 +64,17 @@ def register(request):
     else:
         return render(request, "register.html")
 
-def list(request):
+
+def project_list(request):
     projects = Project.objects.all()
-    return render(request, "list.html", {
+    return render(request, "project_list.html", {
         'projects': projects
     })
 
 
 # Find projects where this user is assigned...
-def dashboard(request):
+# Add an avatar, edit name maybee?
+def dashboard(request, project_id):
     current_user = request.user
     project = Project.objects.get(pk=project_id)
     
@@ -83,30 +85,32 @@ def new_project(request):
     
     if request.method == "POST":
         title = request.POST["title"]
-        current_user = request.user
         project_users_id = request.POST.getlist('users')
-        project_users_list = []
         project_logo = request.POST["project_logo"]
 
         f = Project(title = title, admin = current_user, project_logo = project_logo)
         f.save()
 
+        # Add this project, to the current user list of projects
         current_user.projects.add(f)
         
-        # Add the project to the selected users projects list
-        for user in project_users_id:
-            project_user = User.objects.get(pk=user)
-            project_user.projects.add(f)
 
-            # Retrieve their names and add them to a list, just to display them on the front
-            project_users_list.append( project_user.username )
+        for user_id in project_users_id:
+            # Add the project to the selected users projects list
+            user = User.objects.get(pk=user_id)
+            user.projects.add(f)
 
-        print(project_users_list)
+            # Add the user to the project list of users
+            f.project_users.add(user)
+
+        # Get a QuerySet of the project users
+        project_users = f.project_users.all()
+        # print(project_users_list)
 
         # return this new project page instead
         return render(request, "project.html", {
             "project": f,
-            "project_users_list": project_users_list
+            "project_users": project_users
         })
     else:
         return render(request, "new.html", {
@@ -117,23 +121,62 @@ def new_project(request):
 def project(request, project_id):
     project = Project.objects.get(pk=project_id)
     phases = Phase.objects.filter(project=project)
+    project_users = project.project_users.all()
+
     if request.method == "POST":
-        # Adding a phase
-        name  = request.POST["name"]
-        start_date = request.POST.get("start")
-        end_date = request.POST.get("end")
-        completed = request.POST.get("completed")
-        # completed = True if completed == 'on' else False
-        project = Project.objects.get(pk=project_id)
+        # Is this a request from the Phases form?
+        if 'name' in request.POST:
+            name  = request.POST["name"]
+            start_date = request.POST.get("start")
+            end_date = request.POST.get("end")
+            completed = request.POST.get("completed")
+            completed = True if completed == 'on' else False
+            project = Project.objects.get(pk=project_id)
 
-        f = Phase( name = name, start_date = start_date, end_date = end_date, completed = completed, project = project)
-        f.save()
+            print(completed)
 
-        return render(request, "project.html", {
-            "project": project,
-            "phases": phases
-            # "project_users_list": project_users_list
-        })
+            f = Phase( name = name, start_date = start_date, end_date = end_date, completed = completed, project = project)
+            f.save()
+
+            return render(request, "project.html", {
+                "project": project,
+                "phases": phases,
+                "project_users": project_users
+                # "project_users_list": project_users_list
+            })
+
+        # Then the post is from the Phases toggle button
+        else:
+            # get the clicked on Phase, here, project_id is in fact, phase_id
+            # To Fix this naming
+            if( 'completed' in request.POST ):
+                phase_id = request.POST.get("phase_id")
+                phase = Phase.objects.get(pk=phase_id)
+                phase.completed = False
+
+                phase.save()
+
+                return render(request, "project.html", {
+                    "project": project,
+                    "phases": phases,
+                    "project_users": project_users
+                    # "project_users_list": project_users_list
+                })
+                # "project_users_lipkst": project_users_list
+        
+            else:
+                phase_id = request.POST.get("phase_id")
+                phase = Phase.objects.get(pk=phase_id)
+                phase.completed = True
+
+                phase.save()
+            
+                return render(request, "project.html", {
+                    "project": project,
+                    "phases": phases,
+                    "project_users": project_users
+                    # "project_users_list": project_users_list
+                })
     else:
         try:
             project = Project.objects.get(pk=project_id)
@@ -142,25 +185,37 @@ def project(request, project_id):
 
         return render(request, "project.html", {
             "project": project,
-            "phases": phases
+            "phases": phases,
+            "project_users": project_users
         })
 
     
+# Before editing the project, logo and users must work well
 def edit_project(request, project_id):
     if request.method == "POST":
-        project = Project.objects.get(pk=project_id)
+        # project = Project.objects.get(pk=project_id)
 
-        # Change logo when it works as well
+        # title = request.POST["title"]
+        # current_user = request.user
+        # project_users_id = request.POST.getlist('users')
+        # project_users_list = []
+        # project_logo = request.POST["project_logo"]
+        # # Change logo when it works as well
 
-        title = request.POST["title"]
-        notifications = request.POST["notifications"]
+        # project.title = title
 
-        project.title = request.POST["title"]
-        project.notifications = request.POST["notifications"]
+        # ?
+        # # project.project_users 
 
-        project.save();
+        # title = request.POST["title"]
+       
+
+        # project.title = request.POST["title"]
+        # project.notifications = request.POST["notifications"]
+
+        # project.save();
    
-        return HttpResponseRedirect(reverse('list'))
+        return HttpResponseRedirect(reverse('project_list.html'))
     else:
 
         # Need to create an edit template and pre-populate fields
@@ -169,6 +224,7 @@ def edit_project(request, project_id):
             "project": project
         })
 
+# Projects really need a list of users assigned to that project to work well
 # Make restrictions (edit button, show all projects...Maybe a user level)
 # Able to mark a phase as completed
 # Email notifications? That would be an extra
